c1c349a6ef27a416b8e3e8c6ddb0c239d2f3f52d
Naveenreddy-226 <naveenpeta29@gmail.com>
2025-07-16 22:51:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,48 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const path = require('path');
+const Stripe = require('stripe');
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); // for parsing JSON bodies
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const { items } = req.body;
+
+    const line_items = items.map(item => ({
+      price_data: {
+        currency: 'usd',
+        product_data: { name: item.name },
+        unit_amount: Math.round(item.price * 100), // in cents
+      },
+      quantity: item.qty,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items,
+      mode: 'payment',
+      success_url: 'http://localhost:5173/success',
+      cancel_url: 'http://localhost:5173/',
+    });
+
+    res.json({ id: session.id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
 
 
 app.use('/api/auth', require('./routes/authRoutes'));
