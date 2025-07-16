7229d82fd2b8495030538597cc481fd15b927bdf
Naveenreddy-226 <naveenpeta29@gmail.com>
2025-07-16 22:33:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const path = require('path');
-const Stripe = require('stripe');
 dotenv.config();
 
 connectDB();
 
 const app = express();
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json()); // for parsing JSON bodies
@@ -18,35 +16,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.post('/create-checkout-session', async (req, res) => {
-  const { items } = req.body;
 
-  const line_items = items.map((item) => ({
-    price_data: {
-      currency: 'usd',
-      product_data: {
-        name: item.name,
-      },
-      unit_amount: Math.round(item.price * 100), // Stripe expects cents
-    },
-    quantity: item.qty,
-  }));
-
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items,
-      mode: 'payment',
-      success_url: 'https://shoppingfrontend-vhf1.onrender.com/success',
-      cancel_url: 'https://shoppingfrontend-vhf1.onrender.com',
-    });
-
-    res.json({ id: session.id });
-  } catch (error) {
-    console.error('Stripe Error:', error.message);
-    res.status(500).json({ error: error.message });
-  }
-});
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
